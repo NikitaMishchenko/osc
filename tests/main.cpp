68b4fea580ebe23d678d44d482459f719f0300f1
NikitaMishchenko <bootas@yandex.ru>
2022-12-10 16:06:38 +0700

    gtest appended

---
+++ tests/main.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+
+
+// todo performing queue ???
+
+int makeAllTests()
+{
+    std::cout << "testing stated. Performing...\n";
+
+/*    std::vector<std::pair<bool, uint16_t>> result;
+    result.reserve(5);
+
+    int counter = 0;
+
+    result.emplace_back(EXPECTED_EQ(basic_procedures::FAIL, basic_procedures::performProcedurePeriods("empty_osc", std::string(), std::vector<double>())),
+                        counter);
+
+    counter++;
+
+    std::cout << "Tests were performed!\n";
+    std::cout << "results:\nnumber\tstatus\n";
+    for (auto k : result)
+    {
+        std::cout << k.second << "\t"
+                  << (k.first ? "SUCESS" : "FAIL") << "\n"
+                  << "______________\n";
+    }
+    // basic_procedures::performProcedurePeriods("empty_osc", std::string(), std::vector<double>());
+*/
+    return 0;
+}
+
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
