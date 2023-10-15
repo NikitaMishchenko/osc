2255c149af695926f1c87494dc0cd83b0c0606bb
NikitaMishchenko <bootas@yandex.ru>
2023-10-15 23:11:37 +0700

    moved frequency test

--- tests/test.cpp
+++ tests/test.cpp
@@ -196,41 +196,6 @@ TEST(Test, Amplitude)
     }
 }*/
 
-TEST(Test, Freq)
-{
-    const size_t size = 2000;
-    const double sampleSize = 0.1;
-    const double w = 0.01;
-
-    std::vector<double> time;
-    std::vector<double> signal;
-
-    double tmpSignalValue = 0;
-
-    for (int i = 0; i < size; ++i)
-    {
-        time.push_back(i * sampleSize);
-
-        tmpSignalValue = sin(time.at(i) * w * 2 * M_PI);
-        signal.push_back(tmpSignalValue);
-    }
-
-    AngleHistory angleHistory(time, signal);
-
-    angleHistory.info();
-    // angleHistory.write("testAH");
-
-    WtOscillation wt(angleHistory);
-
-    wt.calcAngleAmplitudeIndexes();
-
-    double wCalculated = wt.getW();
-
-    std::cout << "wCalculated: " << wCalculated << "\n";
-
-    ASSERT_TRUE(abs(w - wCalculated) / w < 0.05); // todo cash presision
-}
-
 
 
 TEST(TestOnGeneratedData, testBasicsInitialisation)
