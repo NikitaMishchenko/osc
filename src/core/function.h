abaac9ef020c742786e4572df6d09e64b6604edb
NikitaMishchenko <bootas@yandex.ru>
2025-03-25 23:26:42 +0700

    Function class fixes

--- src/core/function.h
+++ src/core/function.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <algorithm>
 #include <vector>
+#include <stdexcept>
 
 namespace
 {
@@ -30,24 +31,12 @@ namespace
         double relCmpTolerance;
     };
 
-    class Soreter
-    {
-        Soreter(std::vector<double> xV, std::vector<double> yV)
-        {
-            for (int i = 0; i < xV.size(); i++)
-            {
-                values.push_back(Values(xV.at(i), yV.at(i)));
-            }
-        }
-
-        std::vector<Values> values;
-    };
 }
 
 class Function
 {
 public:
-    Function() : m_domain(std::vector<double>()), m_codomain(std::vector<double>())
+    Function()
     {
     }
 
@@ -58,7 +47,7 @@ public:
           m_codomain(codomain)
     {
         if (m_domain.size() != m_codomain.size())
-            throw "cant construct Function with different domain and codomain sizes!";
+            throw std::range_error("cant construct Function with different domain and codomain sizes!");
     }
 
     Function(const Function &d) : m_domain(d.m_domain), m_codomain(d.m_codomain)
@@ -69,9 +58,12 @@ public:
 
     virtual Function &operator=(const Function &d)
     {
-        m_domain = d.m_domain;
-        m_codomain = d.m_codomain;
-
+        if (this != &d)
+        {
+            m_domain = d.m_domain;
+            m_codomain = d.m_codomain;
+        }
+        
         return *this;
     }
 
@@ -118,6 +110,13 @@ public:
     {
         m_domain.push_back(domain);
         m_codomain.push_back(codomain);
+    }
+
+
+    virtual void emplace_back(const double domain, const double codomain)
+    {
+        m_domain.emplace_back(domain);
+        m_codomain.emplace_back(codomain);
     }
 
     virtual void insert(const std::vector<double> domain, const std::vector<double> codomain)
@@ -198,12 +197,13 @@ private:
 
 inline Function sortFunction(Function func)
 {
-    // Soreter sorter(func.getDomain(), func.getCodomain());
     std::vector<Values> valuesVect;
+
+    valuesVect.reserve(func.size());
 
     for (int i = 0; i < func.size(); i++)
     {
-        valuesVect.push_back(Values(func.getDomain(i), func.getCodomain(i)));
+        valuesVect.emplace_back(Values(func.getDomain(i), func.getCodomain(i)));
     }
     
     const int initialSizeBeforeUnique = valuesVect.size();
@@ -222,9 +222,11 @@ inline Function sortFunction(Function func)
 
     Function resultFnction;
 
+    resultFnction.reserve(valuesVect.size());
+
     for (const auto &v : valuesVect)
     {
-        resultFnction.push_back(v.x, v.y);
+        resultFnction.emplace_back(v.x, v.y);
     }
 
     return resultFnction;
